1f7e5833432d13043992c9551ecaefab2d6b739d
aaronthurston27 <41075873+aaronthurston27@users.noreply.github.com>
2021-02-23 00:09:46 -0800

    Added wall run class.

---
+++ Source/ThirdPersonProject/Private/SpecialMove/TPP_SPM_WallRun.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpecialMove/TPP_SPM_WallRun.h"
+
---
+++ Source/ThirdPersonProject/Public/SpecialMove/TPP_SPM_WallRun.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SpecialMove/TPPSpecialMove.h"
+#include "TPP_SPM_WallRun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONPROJECT_API UTPP_SPM_WallRun : public UTPPSpecialMove
+{
+	GENERATED_BODY()
+	
+};
